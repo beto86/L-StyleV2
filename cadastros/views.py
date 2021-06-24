d3a61e3cf4c68c19554690d0e7ee4bf9c7209163
beto86 <beto.brava86@gmail.com>
2021-06-24 04:45:00 -0300

    criado autenticação por usuario e registro de usuario

--- cadastros/views.py
+++ cadastros/views.py
@@ -3,92 +3,154 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from .models import Endereco, Instituicao, Turma
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
 
-class EnderecoCreate(CreateView):
+class EnderecoCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Endereco
     fields = ['cep', 'rua', 'numero',
               'complemento', 'bairro', 'cidade', 'estado']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-enderecos')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Cadastro de Endereços'
+        return context
 
-class InstituicaoCreate(CreateView):
+
+class InstituicaoCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Instituicao
     fields = ['nome', 'endereco']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-instituicoes')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Cadastro de Instituição'
+        return context
 
-class TurmaCreate(CreateView):
+
+class TurmaCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Turma
     fields = ['nome', 'ra', 'periodo',
               'ano', 'curso', 'turno', 'instituicao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-turmas')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Cadastro de Turma'
+        return context
 
 ################ UPDATE ####################
 
-class EnderecoUpdate(UpdateView):
+
+class EnderecoUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Endereco
     fields = ['cep', 'rua', 'numero',
               'complemento', 'bairro', 'cidade', 'estado']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-enderecos')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Editar Cadastro de Endereço'
+        return context
 
-class InstituicaoUpdate(UpdateView):
+
+class InstituicaoUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Instituicao
     fields = ['nome', 'endereco']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-instituicoes')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Editar Cadastro de Instituição'
+        return context
 
-class TurmaUpdate(UpdateView):
+
+class TurmaUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Turma
     fields = ['nome', 'ra', 'periodo',
               'ano', 'curso', 'turno', 'instituicao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-turmas')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Editar Cadastro de Turma'
+        return context
 
 
 ################ DELETE ####################
 
 
-class EnderecoDelete(DeleteView):
+class EnderecoDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Endereco
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-enderecos')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Excluir Endereço'
+        context['titulo2'] = 'o endereço'
+        return context
 
-class InstituicaoDelete(DeleteView):
+
+class InstituicaoDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Instituicao
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-instituicoes')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Excluir instituição'
+        context['titulo2'] = 'a instituição'
+        return context
 
-class TurmaDelete(DeleteView):
+
+class TurmaDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Turma
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-turmas')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Excluir Turma'
+        context['titulo2'] = 'a turma'
+        return context
 
 
 ################ LIST ####################
 
 
-class EnderecoList(ListView):
+class EnderecoList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Endereco
     template_name = 'cadastros/listas/endereco.html'
 
 
-class InstituicaoList(ListView):
+class InstituicaoList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Instituicao
     template_name = 'cadastros/listas/instituicao.html'
 
 
-class TurmaList(ListView):
+class TurmaList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Turma
     template_name = 'cadastros/listas/turma.html'
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.contrib.auth.models import User
+from .forms import UsuarioForm
+from django.urls import reverse_lazy
 
 # Create your views here.
+
+
+class UsuarioCreate(CreateView):
+    template_name = 'cadastros/form.html'
+    form_class = UsuarioForm
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Registro de Novo Usuário'
+        return context
