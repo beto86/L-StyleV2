e0d308321cf41b30f3009a57d0af799c6c360c6c
beto86 <beto.brava86@gmail.com>
2021-06-25 13:24:23 -0300

    criado a função para atualizar meus dados

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import UsuarioCreate
+from .views import UsuarioCreate, PerfilUpdate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
@@ -8,4 +8,5 @@ urlpatterns = [
     ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('registrar/', UsuarioCreate.as_view(), name='registrar'),
+    path('atualizar-dados/', PerfilUpdate.as_view(), name='atualizar-dados'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 from .models import Perfil
 
 # Create your views here.
@@ -16,10 +17,29 @@ class UsuarioCreate(CreateView):
     def form_invalid(self, form):
         url = super().form_valid(form)
         self.object.save()
+
         Perfil.objects.create(usuario=self.object)
+
         return url
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Registro de Novo Usuário'
+        return context
+
+
+class PerfilUpdate(UpdateView):
+    template_name = 'cadastros/form.html'
+    model = Perfil
+    fields = ['nome_cmpleto', 'cpf', 'telefone',
+              'sexo', 'data_nascimento', 'endereco']
+    success_url = reverse_lazy("index")
+
+    def get_object(self, queryset=None):
+        self.object = get_object_or_404(Perfil, usuario=self.request.user)
+        return self.object
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = 'Meus dados pessoais'
         return context
